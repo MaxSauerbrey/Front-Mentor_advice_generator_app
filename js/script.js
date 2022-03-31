2af1ddf386f52eecc17cc3fd2a7435cdfd9c53b3
Max Sauerbrey <maxsauerbrey@gmail.com>
2022-03-31 18:25:54 -0300

    add js folder

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
-//! Declaracion y asignacion variables DOM
+//! DOM variables declaration and assignment 
 
 const $btnGenerator = document.querySelector(".card__button"),
     $quoteId = document.querySelector(".card__advice-id"),
     $main = document.querySelector(".card"),
     $quote = document.querySelector(".card__advice");
 
+
 async function getData() {
     try {
+        //! res variable await execution of axios request
         let res = await axios.get("https://api.adviceslip.com/advice"),
             json = await res.data;
-        console.log(json);
+        // console.log(json);
+
         $quoteId.innerHTML = `#${json.slip.id}`
         $quote.innerHTML = `${json.slip.advice}`
         $quoteId.appendChild($quoteId)
@@ -18,5 +21,5 @@ async function getData() {
         $quote.innerHTML = `Error ${err.response.status}: ${message}`
     }
 }
-
+//! click event creation 
 $btnGenerator.addEventListener("click", getData);
